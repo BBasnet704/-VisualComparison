eceada7698bfddc6c10c464ec7a158febdb8e568
barsha <barshabasnet2059@gmail.com>
2026-05-21 20:16:31 +0545

    "headless fix"

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   use: {
     baseURL:process.env.BASE_URL!,
     testIdAttribute:'data-test',
-    headless: false,
+     headless: process.env.CI ? true : false,
    viewport: { width: 1280, height: 720 },
    
   },
